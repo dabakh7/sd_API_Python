ecd59595c745d73e4d4f61b188c0c9248be397fa
Dabakh DEME <demeabdou3@gmail.com>
2022-04-04 18:10:12 +0000

    vue modifié

--- vue.py
+++ vue.py
@@ -1,13 +1,11 @@
 from controleur import Utilisateur
 
 api_users = Utilisateur("users")
-# print(api_users.lien_valide()) ###partie Vue
-
-# api_photo = Utilisateur("photos")
-# print(api_photo.lien_valide())
-# api_album = Utilisateur("albums")
-# api_comments = Utilisateur("comments")
-# api_todos = Utilisateur("todos")
+# print(api_users.lien_valide()) 
+api_photo = Utilisateur("photos")
+api_album = Utilisateur("albums")
+api_comments = Utilisateur("comments")
+api_todos = Utilisateur("todos")
 api_posts = Utilisateur("posts")
 donnees_Posts = api_posts.lien_valide()
 
